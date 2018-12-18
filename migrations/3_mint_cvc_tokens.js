d398d303d5e72fde07f6cb76f09cf9e3e44cd53f
Vsevolods Mihailovs <cheelahim@gmail.com>
2018-12-18 13:45:48 +0200

    CVC-919 Provision default IDR account

--- migrations/3_mint_cvc_tokens.js
+++ migrations/3_mint_cvc_tokens.js
@@ -1,24 +1,34 @@
 const TruffleConfig = require('../truffle');
 const { ONE_CVC } = require('../constants');
 const deployedContractHelper = require('./utils/deployedContractHelper');
-const { unlockAccount } = require('./utils');
+const Web3 = require('web3');
 
 module.exports = transform(async (deployer, network, accounts) => {
   const config = TruffleConfig.networks[network];
   const admin = config.from;
   const { gasPrice } = config;
   const getDeployedContract = deployedContractHelper(deployer.provider);
-  unlockAccount(deployer, config);
 
   const token = await getDeployedContract('CvcToken');
+  const web3 = new Web3(deployer.provider);
 
-  // Mint tokens for default requestor addresses
-  accounts.push('0xf91a4ddfa76451d00b703311aae273f2f77cd52c');
-  accounts.push('0x3a8bc151852c3771b5933419e5c74481679789d0');
-  accounts.push('0xa27d4886302c55345a82f94436019e209c5c7bd6');
+  // Provision default IDR:
+  const idr = '0x8935161928e65081bcaef7358e97dce1c560dc9b'; // SIP
+  // Credit tokens
+  const cvcAmount = 1000;
+  await token.transfer(idr, cvcAmount * ONE_CVC, { from: admin, gasPrice });
+  console.log(`${idr} has been credited with ${cvcAmount} CVC`);
+  // Credit ETH
+  const ethAmount = 100;
+  await web3.eth.sendTransaction({
+    from: admin,
+    to: idr,
+    value: web3.toWei(ethAmount, 'ether'),
+    gas: 30000,
+    gasPrice: 100000000000
+  });
+  console.log(`${idr} has been credited with ${ethAmount} ETH`);
 
-  await Promise.all(accounts.map(address => token.transfer(address, 1000 * ONE_CVC, { from: admin, gasPrice })));
-  console.log(`${accounts.length} accounts have been minted 1000 CVCs`);
 });
 
 function transform(callback) {
