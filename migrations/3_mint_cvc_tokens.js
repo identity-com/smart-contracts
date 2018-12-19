7b6740c8f456d2b17b85beb51da9de2dbc7f7932
Vsevolods Mihailovs <cheelahim@gmail.com>
2018-12-19 20:45:48 +0200

    CVC-919 Mint tokens & credit test accounts

--- migrations/3_mint_cvc_tokens.js
+++ migrations/3_mint_cvc_tokens.js
@@ -11,14 +11,9 @@ module.exports = transform(async (deployer, network, accounts) => {
 
   const token = await getDeployedContract('CvcToken');
   const web3 = new Web3(deployer.provider);
-
   // Provision default IDR:
   const idr = '0x8935161928e65081bcaef7358e97dce1c560dc9b'; // SIP
-  // Credit tokens
-  const cvcAmount = 1000;
-  await token.transfer(idr, cvcAmount * ONE_CVC, { from: admin, gasPrice });
-  console.log(`${idr} has been credited with ${cvcAmount} CVC`);
-  // Credit ETH
+  console.log('Crediting ETH...');
   const ethAmount = 100;
   await web3.eth.sendTransaction({
     from: admin,
@@ -29,6 +24,17 @@ module.exports = transform(async (deployer, network, accounts) => {
   });
   console.log(`${idr} has been credited with ${ethAmount} ETH`);
 
+  // Credit tokens
+  console.log('Crediting tokens...');
+  const cvcAmount = 1000;
+  accounts.push(idr);
+  await Promise.all(
+    accounts.map(
+      address => token.transfer(address, cvcAmount * ONE_CVC, { from: admin, gasPrice }).then(() => {
+        console.log(`${address} has been credited with ${cvcAmount} CVC`);
+      })
+    )
+  );
 });
 
 function transform(callback) {
